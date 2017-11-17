21e605733ca26ab640b222c1947f459c2a555f94
nivlac <meffij@gmail.com>
2017-11-17 11:59:29 -0800

    V0 functioning

--- src/index.ts
+++ src/index.ts
@@ -4,47 +4,94 @@ import {
 
 import {
   ICommandPalette
+  , Toolbar
+  , ToolbarButton
 } from '@jupyterlab/apputils';
 
 import {
+  PanelLayout,
   Widget
 } from '@phosphor/widgets';
 
 import '../style/index.css';
 
+class QuiltWidget extends Widget {
+  constructor() {
+    super();
+
+    this.id = 'quilt';
+    this.title.label = 'Quilt Data';
+
+    this.addClass('jp-FileBrowser');
+
+    let layout = new PanelLayout();
+
+    this.toolbar = new Toolbar<Widget>();
+
+    let newthing = new ToolbarButton({
+      className : 'jp-AddIcon'
+    });
+
+    this.toolbar.addItem('New', newthing);
+
+    let search = new Widget();
+    let input = document.createElement('input');
+    input.spellcheck = false;
+    input.className = 'p-CommandPalette-input';
+    search.node.appendChild(input); 
+
+    layout.addWidget(search);
+    layout.addWidget(this.toolbar);
+
+    let results = new Widget();
+    let ul = document.createElement('ul');
+    results.node.appendChild(ul);
+    layout.addWidget(results);
+
+    this.layout = layout;
+
+    function handleSearch() {
+      fetch('https://pkg.quiltdata.com/api/search/?q=' + input.value)
+        .then(response => response.json())
+        .then(json => {
+          // console.log(json);
+          if (json.status === 200) {
+            while (ul.children.length) {
+              ul.removeChild(ul.children.item(0));
+            }
+            json.packages.forEach((e:any) => {
+              let i = document.createElement('ul');
+              i.innerText = e.name;
+              ul.appendChild(i);
+            });
+          };
+        });
+    }
+
+    input.addEventListener("change", handleSearch, false);
+
+
+    this.toolbar.addClass('jp-FileBrowser-toolbar');
+
+  }
+
+  readonly toolbar : Toolbar<Widget>;
+
+}
 
 /**
  * Initialization data for the jupyterlab_xkcd extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_xkcd',
+  id: 'Quilt',
   autoStart: true,
   requires: [ICommandPalette],
   activate: (app, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+    console.log('JupyterLab extension Quilt is activated!');
 
-    // Create a single widget
-    let widget: Widget = new Widget();
-    widget.id = 'xkcd-jupyterlab';
-    widget.title.label = 'xkcd.com';
-    widget.title.closable = true;
+    let widget = new QuiltWidget();
 
-    // Add an application command
-    const command: string = 'xkcd:open';
-    app.commands.addCommand(command, {
-      label: 'Random xkcd comic',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main area if it's not there
-          app.shell.addToMainArea(widget);
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    // Add the command to the palette.
-    palette.addItem({command, category: 'Tutorial'});
+    app.shell.addToLeftArea(widget);
   }
 };
 
